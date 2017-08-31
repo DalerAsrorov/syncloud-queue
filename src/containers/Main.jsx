37d814dc31a50b5a92adc7efb6450864d34ab641
DalerAsrorov <asrorids@gmail.com>
2017-08-31 00:53:23 -0700

    Initial layout fixed

---
+++ src/containers/Main.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import MainView from '../components/MainView';
+
+export default class Main extends Component {
+	render() {
+		return <MainView />;
+	}
+}
