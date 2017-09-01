7229dbb8c4e5271fe7e44e51bbdbf2b855010811
Daler Asrorov <asrorids@gmail.com>
2017-09-01 15:46:50 -0700

    A better solution for filling width for search

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -4,33 +4,31 @@ import React from 'react';
 import styled from 'styled-components';
 
 const SearchInput = styled.input`
-	width: 100%;
-	padding: 5px;
-	font-size: 28px;
-	border-radius: 0;
-	width: -webkit-fill-available;
-	border-radius: 0 !important;
-	border-width: 1px;
-	border-style: groove;
-	transition: box-shadow 0.15s ease;
+    width: 100%;
+    padding: 5px;
+    font-size: 28px;
+    border-radius: 0;
+    border-radius: 0 !important;
+    border-width: 1px;
+    border-style: groove;
+    transition: box-shadow 0.15s ease;
 
-	&:hover {
-		box-shadow: 0 0 13px #e49a53;
-		border-style: groove;
-		border-color: #ffe4c9;
-	}
+    &:hover {
+        box-shadow: 0 0 13px #e49a53;
+        border-style: groove;
+        border-color: #ffe4c9;
+    }
 
-	&:focus {
-		outline: 1px solid #e49a53;
-	}
+    &:focus {
+        outline: 1px solid #e49a53;
+    }
 `;
 
-const SearchFormWrapper = styled.form``;
+const SearchFormWrapper = styled.form`display: flex;`;
 
-const SearchForm = (props: { onSubmit: Function, onChange: Function }) => (
-	<SearchFormWrapper onSubmit={props.onSubmit}>
-		<SearchInput onChange={props.onChange} placeholder="Search artists, playlists, tracks..." name="search" />
-	</SearchFormWrapper>
-);
+const SearchForm = (props: { onSubmit: Function, onChange: Function }) =>
+    <SearchFormWrapper onSubmit={props.onSubmit}>
+        <SearchInput onChange={props.onChange} placeholder="Search artists, playlists, tracks..." name="search" />
+    </SearchFormWrapper>;
 
 export default SearchForm;
