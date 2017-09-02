2b9dfde59bc7218594953fb4956928ecc3f79a6e
Daler Asrorov <asrorids@gmail.com>
2017-09-01 23:37:32 -0700

    Created a progress bar

---
+++ src/containers/Player/Progress.jsx
@@ -0,0 +1,27 @@
+// @flow
+
+import React, { Component } from 'react';
+import styled from 'styled-classnames';
+import { Progress as ProgressBar, Icons } from 'react-soundplayer/components';
+
+const progressClassName = styled` 
+    cursor: pointer;
+    width: 100%;
+    height: 10px;
+    background: #ddd; 
+    
+    & > div {
+        background: ${props => props.color};
+        height: 100%;  
+    }
+`;
+
+type Props = {
+    color: string
+};
+
+export default class Progress extends Component<Props, {}> {
+    render() {
+        return <ProgressBar className={progressClassName(this.props)} {...this.props} />;
+    }
+}
--- src/containers/Player/index.jsx
+++ src/containers/Player/index.jsx
@@ -5,10 +5,13 @@ import styled from 'styled-components';
 import { getClientID } from '../../api';
 import { SoundPlayerContainer } from 'react-soundplayer/addons';
 import PlayPause from './PlayPause';
+import Progress from './Progress';
 
 const PlayerWrapper = styled.article``;
 const BASE_COLOR = '#e49a53';
 const COLOR = '#fff';
+const TEXT_COLOR = '#000';
+
 const RESOLVE_URL_TEST = 'https://soundcloud.com/daler-asrorov/daler-asrorov-not-a-lullaby-preview-demo';
 
 export default class Player extends Component<{}, {}> {
@@ -21,6 +24,7 @@ export default class Player extends Component<{}, {}> {
             <PlayerWrapper>
                 <SoundPlayerContainer clientId={getClientID()} resolveUrl={RESOLVE_URL_TEST} onReady={this._trackReady}>
                     <PlayPause background={BASE_COLOR} color={COLOR} />
+                    <Progress color={BASE_COLOR} />
                 </SoundPlayerContainer>
             </PlayerWrapper>
         );
