adf9236bc8b66518ddb94f2e00fed7c94de0d540
Daler Asrorov <asrorids@gmail.com>
2017-09-02 01:04:42 -0700

    Improved nav bar with logo

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,37 +4,45 @@ import React from 'react';
 import styled from 'styled-components';
 import FaGithub from 'react-icons/lib/fa/github';
 
-const NavTitle = styled.h2`
-	display: inline-block;
-	width: 85%;
-`;
+import logo from '../images/syncloud.png';
 
 const IconLinkWrapper = styled.a`
-	text-decoration: none;
-	font-size: 2.5em;
-	display: inline-block;
-	color: #000;
-	transition: color 0.3s ease-in;
+    display: inline-flex;
+    vertical-align: sub;
+    text-decoration: none;
+    font-size: 2.5em;
+    float: right;
+    color: #000;
+    transition: color 0.3s ease-in;
 
-	&:hover {
-		color: #e49a53;
-	}
+    &:hover {
+        color: #e49a53;
+    }
+
+    & > svg {
+        vertical-align: baseline !important;
+    }
 `;
 
 const NavWrapper = styled.nav`
-	background: #fff;
-	padding: 1px 15px;
+    background: #fff;
+    padding: 1px 15px;
+`;
+
+const HeaderImage = styled.img`
+    height: 40px;
+    vertical-align: sub;
 `;
 
 const REPO_LINK = 'https://github.com/DalerAsrorov/syncloud-queue';
 
 const Nav = (props: {}) => (
-	<NavWrapper>
-		<NavTitle> Syncloud Queue</NavTitle>
-		<IconLinkWrapper target="__blank" href={REPO_LINK}>
-			<FaGithub />
-		</IconLinkWrapper>
-	</NavWrapper>
+    <NavWrapper>
+        <HeaderImage src={logo} alt="SyncloudQueue Logo" />
+        <IconLinkWrapper target="__blank" href={REPO_LINK}>
+            <FaGithub />
+        </IconLinkWrapper>
+    </NavWrapper>
 );
 
 export default Nav;
