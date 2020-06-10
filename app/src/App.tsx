bfc5c4decc66e65d8e73e1be2483694793999e4a
DalerAsrorov <asrorids@gmail.com>
2020-06-09 23:34:59 -0400

    More great UI layout stuff

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,21 +1,44 @@
-import React from 'react';
-// import logo from './logo.svg';
-import {Container, Grid, Segment} from 'semantic-ui-react';
+import React from "react";
+import { Container, Grid, Image, Segment, Input } from "semantic-ui-react";
+import appLogo from "./images/syncloud.png";
+import githubLogo from "./images/github.png";
 
-export const App = () => {
-  return (
-    <Container fluid content as={Segment}>
-      <Segment>navigation bar</Segment>
-      <Grid>
-        <Grid.Row divided>
-          <Grid.Column width={6}>
-            <Segment>Left side container for player</Segment>
-          </Grid.Column>
-          <Grid.Column width={10}>
-            <Segment>Right side container for search and playlists</Segment>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    </Container>
-  );
-}
+export const App = () => (
+  <Container fluid as={Segment}>
+    <Grid as={Segment} style={{ marginTop: 0 }}>
+      <Grid.Column floated="left" width={5}>
+        <Image
+          src={appLogo}
+          style={{ width: "4rem" }}
+          size="mini"
+          floated="left"
+        />
+      </Grid.Column>
+      <Grid.Column floated="right" width={5}>
+        <Image
+          href="https://github.com/DalerAsrorov/syncloud-queue"
+          target="__blank"
+          style={{ width: "3rem" }}
+          src={githubLogo}
+          size="mini"
+          floated="right"
+        />
+      </Grid.Column>
+    </Grid>
+    <Grid>
+      <Grid.Row>
+        <Grid.Column width="16">
+          <Input size="huge" fluid icon="search" placeholder="Search..." />
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row divided>
+        <Grid.Column width={6}>
+          <Segment>Left side container for player</Segment>
+        </Grid.Column>
+        <Grid.Column width={10}>
+          <Segment>Right side container for search and playlists</Segment>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  </Container>
+);
