8fabeaa54fb5aa39455d43f742d89a0264d5d073
DalerAsrorov <asrorids@gmail.com>
2020-06-13 02:34:31 -0400

    Some styling improvements

--- app/src/components/ListOfTracks.tsx
+++ app/src/components/ListOfTracks.tsx
@@ -1,4 +1,3 @@
-import { toJS } from 'mobx';
 import { observer } from 'mobx-react';
 import React from 'react';
 import { Container, Header, Icon, Segment } from 'semantic-ui-react';
--- app/src/components/Player/index.tsx
+++ app/src/components/Player/index.tsx
@@ -2,22 +2,45 @@
 
 import React from 'react';
 import { withSoundCloudAudio } from 'react-soundplayer/addons';
-import { PlayButton, Timer } from 'react-soundplayer/components';
+import { PlayButton, Timer, Progress } from 'react-soundplayer/components';
+import classNames from 'styled-classnames';
 import { Grid, Segment } from 'semantic-ui-react';
 
 type PlayerProps = any;
+
+const playButtonStyleClass = classNames`
+  width: 100%;
+  height: 100%;
+  cursor: pointer;
+  padding: 1rem;
+  border: none;
+  outline: none;
+  background: ${(props: { artwork: string }) => `url(${props.artwork})`};
+  color: #fff;
+  background-size: cover;
+
+  & > svg {
+    width: 50%;
+    height: 50%;
+  }
+`;
 
 export const Player: React.FC<PlayerProps> = withSoundCloudAudio(
   (props: PlayerProps) => {
     const { track, currentTime } = props;
     const { nextTrackID } = track;
+    const artwork = track.artwork_url
+      ? track.artwork_url
+      : track.user.avatar_url;
+
+    console.log(track);
 
     return (
       <Grid as={Segment} verticalAlign="middle">
         <Grid.Row>
           <Grid.Column width={4} stretched>
             <PlayButton
-              style={{ cursor: 'pointer' }}
+              className={playButtonStyleClass({ artwork })}
               onPlayClick={() => {
                 console.log('play button clicked!');
               }}
@@ -25,9 +48,8 @@ export const Player: React.FC<PlayerProps> = withSoundCloudAudio(
             />
           </Grid.Column>
           <Grid.Column width={9} stretched>
-            <h2 className="custom-player-title">
-              {track ? track.title : 'Loading...'}
-            </h2>
+            <h3>{track.title}</h3>
+            <Progress {...props} />
           </Grid.Column>
           <Grid.Column width={3} stretched>
             <Timer
